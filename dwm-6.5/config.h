f6b5376ab88d0147e2b52818c9fa33fc23542b5d
lovemearowlet <158019744+lovemearowlet@users.noreply.github.com>
2024-10-16 23:23:26 -0400

    Update config.h

--- dwm-6.5/config.h
+++ dwm-6.5/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -11,7 +11,7 @@ static const char col_gray1[]       = "#1e1e1e";
 static const char col_gray2[]       = "#232323";
 static const char col_gray3[]       = "#7e7e7e";
 static const char col_gray4[]       = "#b3b3b3";
-static const char col_cyan[]        = "#202020";
+static const char col_cyan[]        = "#501fa6";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
